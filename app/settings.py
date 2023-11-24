b9a2e930c88ee5484943f7147625556922588bc0
zombie <yacaeh@gmail.com>
2023-11-25 07:55:33 +0900

    update statics

--- app/settings.py
+++ app/settings.py
@@ -187,14 +187,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 # Additional locations of static files
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+path.join(BASE_DIR, 'media')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
